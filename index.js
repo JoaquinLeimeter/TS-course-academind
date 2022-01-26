6339ffa347d25197bde1a0cf359fe45b8b89745a
Joaquin leimeter <leimeter.joaquin@gmail.com>
2022-01-26 13:49:28 -0300

    TUPLE

--- index.js
+++ index.js
@@ -12,18 +12,34 @@
 // 	age: 29
 // }
 // console.log(person.name);
-ARRAYS;
+// ARRAYS
+// const person = {
+// 	name : 'david',
+// 	age: 29,
+// 	hobbies: ['videogames, cooking']
+// };
+// let favoriteActivities: string[];
+// favoriteActivities = ['string'];
+// console.log(person.name);
+// for (const hobby of person.hobbies) {
+// 	console.log(hobby.toLowerCase()); //autocompletion
+// 	console.log(hobby.map(() => {})) //not an array
+// };
+//Tuple
 var person = {
     name: 'david',
     age: 29,
-    hobbies: ['videogames, cooking']
+    hobbies: ['videogames, cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin'); //exeption for .push()
+// person.role[1] = 10; //doesnt work
 var favoriteActivities;
 favoriteActivities = ['string'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toLowerCase()); //autocompletion
-    console.log(hobby.map(function () { })); //not an array
+    // console.log(hobby.map(() => {})) //not an array
 }
 ;
--- index.ts
+++ index.ts
@@ -15,13 +15,41 @@
 
 // console.log(person.name);
 
-ARRAYS
+// ARRAYS
 
-const person = {
+// const person = {
+// 	name : 'david',
+// 	age: 29,
+// 	hobbies: ['videogames, cooking']
+// };
+
+// let favoriteActivities: string[];
+// favoriteActivities = ['string'];
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+// 	console.log(hobby.toLowerCase()); //autocompletion
+// 	console.log(hobby.map(() => {})) //not an array
+// };
+
+//Tuple
+
+const person: {
+	name: string;
+	age: number;
+	hobbies: string[];
+	role: [number, string];
+} = {
 	name : 'david',
 	age: 29,
-	hobbies: ['videogames, cooking']
+	hobbies: ['videogames, cooking'],
+	role: [2, 'author']
 };
+
+person.role.push('admin'); //exeption for .push()
+// person.role[1] = 10; //doesnt work
+
 
 let favoriteActivities: string[];
 favoriteActivities = ['string'];
@@ -30,5 +58,5 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
 	console.log(hobby.toLowerCase()); //autocompletion
-	console.log(hobby.map(() => {})) //not an array
+	// console.log(hobby.map(() => {})) //not an array
 };
