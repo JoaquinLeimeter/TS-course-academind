63d3d5e06d4f1b63df35819bd14ce292a536698b
Joaquin leimeter <leimeter.joaquin@gmail.com>
2022-01-26 14:32:53 -0300

    UNION TYPES

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
-//OBJECTS
-// const person: {
-// 	name: string;
-// 	age: number;
-// } = {
-// 	name : 'david',
-// 	age: 29
-// }
-// 
-// const person = {
-// 	name : 'david',
-// 	age: 29
-// }
-// console.log(person.name);
-// ARRAY
-// const person = {
-// 	name : 'david',
-// 	age: 29,
-// 	hobbies: ['videogames, cooking']
-// };
-// let favoriteActivities: string[];
-// favoriteActivities = ['string'];
-// console.log(person.name);
-// for (const hobby of person.hobbies) {
-// 	console.log(hobby.toLowerCase()); //autocompletion
-// 	console.log(hobby.map(() => {})) //not an array
-// };
-//TUPLE
-// const person: {
-// 	name: string;
-// 	age: number;
-// 	hobbies: string[];
-// 	role: [number, string];
-// } = {
-// 	name : 'david',
-// 	age: 29,
-// 	hobbies: ['videogames, cooking'],
-// 	role: [2, 'author']
-// };
-// person.role.push('admin'); //exeption for .push()
-// // person.role[1] = 10; //doesnt work
-//ENUM
-// enum Role {
-// 	ADMIN = 1, READ_ONLY, AUTHOR
-// };
-// const person = {
-// 	name : 'david',
-// 	age: 29,
-// 	hobbies: ['videogames, cooking'],
-// 	role: Role.ADMIN
-// };
-// console.log(person.role);
-//ANY
-//avoid 'any' basicaly
-var array;
-array = 1; //it's an array!
-console.log(array);
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combinedAges = combine(30, 36);
+console.log(combinedAges);
+var combinedNames = combine('Joaquin', 'german');
+console.log(combinedNames);
--- index.ts
+++ index.ts
@@ -1,75 +1,15 @@
-//OBJECTS
+function combine(input1: number | string, input2: number | string) {
+	let result;
+	if(typeof input1 === 'number' && typeof input2 === 'number'){
+		result = input1 + input2;
+	} else {
+		result = input1.toString() + input2.toString();
+	}
+	return result;
+}
 
-// const person: {
-// 	name: string;
-// 	age: number;
-// } = {
-// 	name : 'david',
-// 	age: 29
-// }
-// 
-// const person = {
-// 	name : 'david',
-// 	age: 29
-// }
+const combinedAges = combine(30,36);
+console.log(combinedAges);
 
-// console.log(person.name);
-
-// ARRAY
-
-// const person = {
-// 	name : 'david',
-// 	age: 29,
-// 	hobbies: ['videogames, cooking']
-// };
-
-// let favoriteActivities: string[];
-// favoriteActivities = ['string'];
-
-// console.log(person.name);
-
-// for (const hobby of person.hobbies) {
-// 	console.log(hobby.toLowerCase()); //autocompletion
-// 	console.log(hobby.map(() => {})) //not an array
-// };
-
-//TUPLE
-
-// const person: {
-// 	name: string;
-// 	age: number;
-// 	hobbies: string[];
-// 	role: [number, string];
-// } = {
-// 	name : 'david',
-// 	age: 29,
-// 	hobbies: ['videogames, cooking'],
-// 	role: [2, 'author']
-// };
-
-// person.role.push('admin'); //exeption for .push()
-// // person.role[1] = 10; //doesnt work
-
-//ENUM
-
-// enum Role {
-// 	ADMIN = 1, READ_ONLY, AUTHOR
-// };
-
-// const person = {
-// 	name : 'david',
-// 	age: 29,
-// 	hobbies: ['videogames, cooking'],
-// 	role: Role.ADMIN
-// };
-
-// console.log(person.role);
-
-//ANY
-
-//avoid 'any' basicaly
-
-let array: any[];
-array = 1; //it's an array!
-
-console.log(array);
+const combinedNames =  combine('Joaquin', 'german');
+console.log(combinedNames);
